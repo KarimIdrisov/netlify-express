00bbd2a9d0d605bd4541e98e9cb67cb9ba898304
Karim Idrisov <karim.idrisow2016@gmail.com>
2022-06-19 02:09:46 +1000

    test

--- src/create-response.js
+++ src/create-response.js
@@ -185,7 +185,7 @@ module.exports = {
     };
   },
   startTest: function ({ request, session, version }) {
-    if (questionId > 2) {
+    if (questionId > 1) {
       if (questions[questionId - 2]["right"].toLowerCase() === lastAnswer) {
         return {
           response: {
